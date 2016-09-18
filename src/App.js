7cc7493ebe5d4f3c61bc0aa39d71bd40c0e8a1fd
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2016-09-18 23:44:00 +0300

    Add React on top of that, hook it together

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import _ from 'lodash'
+import React, { Component } from 'react'
+import Horizon from '@horizon/client'
+
+import logo from './logo.svg'
+import './App.css'
+
+const horizon = Horizon({ host: 'localhost:8181' })
+
+horizon.status().subscribe(
+  status => console.log('Horizon status', status)
+)
+
+horizon.connect()
+
+class App extends Component {
+  state = {
+    messages: [],
+  }
+
+  componentDidMount() {
+    horizon('messages').order('timestamp', 'descending')
+      .watch({ rawChanges: true }).subscribe(message => {
+        if (!_.includes(['initial', 'add'], message.type)) {
+          return
+        }
+
+        console.log('.')
+
+        let { messages } = this.state
+        messages = _.concat(messages, message.new_val)
+        this.setState({ messages })
+      })
+  }
+
+  render() {
+    const { messages } = this.state
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="App-intro">
+          {_.map(messages, m => {
+            return <div key={m.id}>{m.from} -> {m.to}: {m.text}</div>
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
