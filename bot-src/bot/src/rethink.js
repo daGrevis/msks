1b85695b3018535f2fadb911bed8245d8fdcaa53
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2016-10-13 20:28:28 +0300

    Also rename db to r in rethink.js

--- bot-src/bot/src/rethink.js
+++ bot-src/bot/src/rethink.js
@@ -2,18 +2,18 @@ const Promise = require('bluebird')
 const _ = require('lodash')
 const rethinkdb = require('rethinkdbdash')
 
-const db = rethinkdb({ silent: true })
+const r = rethinkdb({ silent: true })
 
 const queries = [
-  db.tableCreate('channels', { primaryKey: 'name' }),
+  r.tableCreate('channels', { primaryKey: 'name' }),
 
-  db.tableCreate('active_users'),
-  db.table('active_users').indexCreate('username'),
-  db.table('active_users').indexCreate('channel'),
+  r.tableCreate('active_users'),
+  r.table('active_users').indexCreate('username'),
+  r.table('active_users').indexCreate('channel'),
 
-  db.tableCreate('messages'),
-  db.table('messages').indexCreate('from'),
-  db.table('messages').indexCreate('timestamp'),
+  r.tableCreate('messages'),
+  r.table('messages').indexCreate('from'),
+  r.table('messages').indexCreate('timestamp'),
 ]
 
 // Run queries in sequence, but never fail.
@@ -22,4 +22,4 @@ Promise.each(
   _.noop
 )
 
-module.exports = db
+module.exports = r
