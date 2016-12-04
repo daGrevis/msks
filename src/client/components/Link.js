8b3b5fad71a2f4c4d7c02acfe5b7e30b71a7959a
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2016-12-04 17:48:30 +0200

    Add basePath and set homepage in package.json

--- src/client/components/Link.js
+++ src/client/components/Link.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import { navigate } from '../history'
 
+const basePath = process.env.REACT_APP_BASE_PATH || ''
+
 function Link(props) {
   return (
-    <span className='link' onClick={() => navigate(props.href)}>{props.children}</span>
+    <span className='link' onClick={() => navigate(basePath + props.href)}>{props.children}</span>
   )
 }
 
