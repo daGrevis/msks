bef460793329ad98a01e2601fd5983dc42a08646
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2016-12-04 21:38:58 +0200

    Obfuscate config when logging

--- bot-src/bot/src/config.js
+++ bot-src/bot/src/config.js
@@ -1,6 +1,10 @@
+const fp = require('lodash/fp')
+
+const obfuscateConfig = fp.set('ircPassword', '***')
+
 try {
   const config = require('../config.json')
-  console.log('config loaded', config)
+  console.log('config loaded', obfuscateConfig(config))
   module.exports = config
 } catch(err) {
   console.info('Config not found or invalid, see config.example.json')
