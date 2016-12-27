d7d845302d42656c6a44e4704b6eb4f99c840684
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2016-12-27 23:01:03 +0200

    Use md5 for nick colors

--- src/client/colors.js
+++ src/client/colors.js
@@ -1,32 +1,26 @@
-import fp from 'lodash/fp'
-
-const seed = 7
+import bigInt from 'big-integer'
+import md5 from 'js-md5'
 
 const colors = [
-  '#e91e63',
   '#9c27b0',
-  '#3f51b5',
   '#03a9f4',
   '#00bcd4',
   '#009688',
-  '#8bc34a',
-  '#cddc39',
-  '#ffc107',
-  '#ff9800',
-  '#ff5722',
+  '#e91e63',
   '#795548',
+  '#ff9800',
+  '#cddc39',
+  '#3f51b5',
+  '#8bc34a',
+  '#ff5722',
+  '#ffc107',
 ]
 
-const getColorHash = s => (
-  fp.reduce((sum, c) => sum + c.charCodeAt(0), seed, s.split(''))
-)
-
 const getColor = s => (
-  colors[getColorHash(s) % colors.length]
+  colors[bigInt(md5(s), 16).mod(colors.length)]
 )
 
 export {
   colors,
-  getColorHash,
   getColor,
 }
