fbdcca3e35857add41a47c92a40ca7c06212e406
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2016-12-28 02:39:09 +0200

    Expose SOCKETIO_PATH build option

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client'
 
 const url = process.env.REACT_APP_SOCKETIO_URL || '//:3001'
+const path = process.env.REACT_APP_SOCKETIO_PATH || '/socket.io'
 
-const socket = io(url)
+const socket = io(url, { path })
 
 export default socket
