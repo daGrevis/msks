d5fe600a69a574128c79df00bba8b39755044b04
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2017-09-26 22:14:30 +0300

    Upgrade Socket.IO, default to websocket transport

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,6 +2,13 @@ import io from 'socket.io-client'
 
 import config from './config'
 
-const socket = io(config.socketUrl, { path: config.socketPath })
+const socket = io(config.socketUrl, {
+  path: config.socketPath,
+  transports: ['websocket'],
+})
+
+socket.on('reconnect_attempt', () => {
+  socket.io.opts.transports = ['polling', 'websocket']
+})
 
 export default socket
