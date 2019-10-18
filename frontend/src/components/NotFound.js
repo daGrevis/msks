024e092c1a86a3abff8881762ee735fc6a251824
Raitis Stengrevics <dagrevis@gmail.com>
2019-10-18 21:47:16 +0300

    Fix 404 page link

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Link from './Link'
+import { getLinkToFront } from '../routing/links'
 
 import '../styles/NotFound.css'
 
 const NotFound = () => (
-  <Link href="/">
+  <Link href={getLinkToFront()}>
     <div className="NotFound">404</div>
   </Link>
 )
